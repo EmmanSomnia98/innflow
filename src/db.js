5970f382ce6dbb963e653a07280034dabc901748
Emmanuel Alexandre Maribbay <adammaribbay121198@gmail.com>
2025-10-17 20:59:15 +0800

    Fix server error for request ID rtw8t-1760705134245-c66d647d3346

--- src/db.js
+++ src/db.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI ||
-        'mongodb://127.0.0.1:27017/mvc_crud_example', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log('膆 MongoDB connected');
+        console.log('MongoDB connected');
     } catch (err) {
-    console.error('MongoDB connection failed', err);
-    process.exit(1);
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1); // Exit the process if the connection fails
     }
 };
 

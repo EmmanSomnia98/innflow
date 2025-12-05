2fb2da09933ae6a2bb8d4cee3dc1937174f6d5e9
Emmanuel Alexandre Maribbay <adammaribbay121198@gmail.com>
2025-12-05 09:06:04 +0800

    Update bookingRoutes.js

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bookingControllers = require('../controllers/bookingControllers');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+// Apply authentication middleware to all booking routes
+router.use(authMiddleware);
 
 router.get('/bookings', bookingControllers.getBookings);
 router.get('/bookings/:id', bookingControllers.getBooking);

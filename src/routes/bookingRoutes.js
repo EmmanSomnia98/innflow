bcca526ea18f9687dfc77c26e3d001dffc58e7f9
Emmanuel Alexandre Maribbay <adammaribbay121198@gmail.com>
2025-12-05 09:46:17 +0800

    Fix bookigRoutes.js error

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bookingControllers = require('../controllers/bookingControllers');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-// Apply authentication middleware to all booking routes
-router.use(authMiddleware);
 
 router.get('/bookings', bookingControllers.getBookings);
 router.get('/bookings/:id', bookingControllers.getBooking);
